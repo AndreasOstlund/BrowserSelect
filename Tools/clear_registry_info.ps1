20088f6e7e2ac2e6759757b64a8b19384f176c62
AndreasOstlund <AndreasOstlund@users.noreply.github.com>
2018-07-16 21:01:03 +0200

    Extended remove-script with a function to set suffix

--- Tools/clear_registry_info.ps1
+++ Tools/clear_registry_info.ps1
@@ -1,8 +1,22 @@
 ﻿#
 # For testing/debugging purposes, clear BrowserSelect registry info.
 #
-Remove-Item -Path HKCU:\Software\BrowserSelect 
-Remove-item -Path HKCU:\Software\Clients\StartMenuInternet\BrowserSelect.exe -Recurse
-Remove-ItemProperty -Path HKCU:\Software\RegisteredApplications -Name "BrowserSelect"
-Remove-Item -Path HKCU:\Software\Classes\bselectURL -Recurse
+Function Remove-BrowserSelectToBrowserList {
+    [cmdletBinding(SupportsShouldProcess=$True)]
+    Param(
+        [string]$BrowserSuffix
+    )
+
+    $BrowserRegName="BROWSERSELECT${BrowserSuffix}"
+    $HKCUStartMenuInternet = "Software\Clients\StartMenuInternet\$BrowserRegName"
+    $HKCUStartMenuInternetFullPath = "HKCU:\$HKCUStartMenuInternet"
+
+    Remove-Item -Path HKCU:\Software\$BrowserRegName 
+    Remove-item -Path $HKCUStartMenuInternetFullPath -Recurse
+    Remove-ItemProperty -Path HKCU:\Software\RegisteredApplications -Name $BrowserRegName
+    Remove-Item -Path HKCU:\Software\Classes\bselectURL -Recurse
+
+}
+
+Remove-BrowserSelectToBrowserList -BrowserSuffix ".exe" -Verbose
 
